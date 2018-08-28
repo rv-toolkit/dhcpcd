c40856b225ad4c2930c68b82275581ea9d9f9d4c
yiyiwang <shd101wyy@sina.com>
2018-08-28 11:30:20 -0500

    Fix rv.sh

--- rv.sh
+++ rv.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+make clean
 json_out=`pwd`/errors.json
-./configure CC=kcc CFLAGS="-D__packed='__attribute__((packed))' -frecover-all-errors -fissue-report=$json_out" LD=kcc
+report_out=`pwd`/report
+rm $json_out
+export CC=kcc
+export CFLAGS="-D__packed='__attribute__((packed))' -frecover-all-errors -fissue-report=$json_out"
+export CPPFLAGS="-fissue-report=$json_out"
+export LD=kcc
+./configure
 make -j`nproc`
+make tests
+rv-html-report $json_out -o $report_out
+rv-upload-report $report_out
